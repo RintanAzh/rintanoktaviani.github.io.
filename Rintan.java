d680a6a3c08ca362677b89bab114663afc309c50
RintanAzh <71128928+RintanAzh@users.noreply.github.com>
2020-09-11 18:57:43 +0700

    Add files via upload

---
+++ Rintan.java
@@ -0,0 +1,16 @@
+package rintan;
+
+public class Rintan {
+    public static void main (String[]args){
+String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+System.out.println(alfabet.charAt(17));
+System.out.println(alfabet.charAt(8));
+System.out.println(alfabet.charAt(13));
+System.out.println(alfabet.charAt(19));
+System.out.println(alfabet.charAt(0));
+System.out.println(alfabet.charAt(13));
+
+
+}
+}
